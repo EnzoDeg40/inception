ce4ffc42db9839e6b0240a6a04a40d87af9d2c15
Enzo Degraeve <enzodeg40@gmail.com>
2024-10-14 15:47:10 +0200

    poop2

--- scrs/requirements/wordpress/setup.sh
+++ scrs/requirements/wordpress/setup.sh
@@ -1,47 +1,24 @@
-#!/bin/bash
+#!/bin/sh
 
-# Debug
-set -xe 
-
-if [ -f /var/www/html/wp-config.php ];
+#check if wp-config.php exist
+if [ -f ./wp-config.php ]
 then
 	echo "wordpress already downloaded"
 else
 
-# mkdir -p /var/www/html
+	#Download wordpress and all config file
+	wget http://wordpress.org/latest.tar.gz
+	tar xfz latest.tar.gz
+	mv wordpress/* .
+	rm -rf latest.tar.gz
+	rm -rf wordpress
 
-# wget https://wordpress.org/latest.tar.gz \
-#     && tar -xvzf latest.tar.gz -C /var/www/html --strip-components=1 \
-#     && rm latest.tar.gz
-
-# chown -R www-data:www-data /var/www/html
-
-
-# copie le fichier de xconfiguration de PHP
-# sed -i "s/username_here/${MARIA_USER}/g" /var/www/html/wp-config.php
-# sed -i "s/password_here/${MARIA_PASSWORD}/g" /var/www/html/wp-config.php
-# sed -i "s/localhost/mariadb/g" /var/www/html/wp-config.php
-# sed -i "s/database_name_here/${MARIA_DATABASE}/g" /var/www/html/wp-config.php
-
-# install wp-cli
-wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-chmod +x wp-cli.phar
-mv wp-cli.phar /usr/local/bin/wp
-
-# RUN wp config create --dbname=wordpress --dbuser=root --dbpass=motdepasse --dbhost=localhost
-# https://chatgpt.com/share/67092a63-02a4-800e-8687-d53f808fbba6
-# RUN wp core install --url=localhost --title=enzo_wp --admin_user=admin --admin_password=admin --admin_email=admin@example.com --allow-root --path=/var/www/html
-
-# Cree le fichier de configuration de wordpress
-wp config create --dbname=${MARIA_DATABASE} --dbuser=${MARIA_USER} --dbpass=${MARIA_PASSWORD} --dbhost=mariadb --allow-root --path=/var/www/html --extra-php <<PHP
-define( 'WP_DEBUG', true );
-define( 'WP_DEBUG_LOG', true );
-PHP
-
-# Installe wordpress
-wp core install --url=https://localhost/ --title=Example --admin_user=supervisor --admin_email=info@example.com --admin_password=strongpassword --allow-root --path=/var/www/html
-
-
+	#Inport env variables in the config file
+	sed -i "s/username_here/enzo/g" wp-config-sample.php
+	sed -i "s/password_here/enzo/g" wp-config-sample.php
+	sed -i "s/localhost/mariadb/g" wp-config-sample.php
+	sed -i "s/database_name_here/wp/g" wp-config-sample.php
+	cp wp-config-sample.php wp-config.php
 fi
 
-exec "$@" # a quoi il sert ?2
+exec "$@"
