e58c2112e37a9d39bcffbea863a1400c0d9ec93c
Enzo Degraeve <enzodeg40@gmail.com>
2024-10-15 10:07:12 +0200

    Success install wp

--- scrs/requirements/wordpress/setup.sh
+++ scrs/requirements/wordpress/setup.sh
@@ -4,36 +4,25 @@
 set -xe 
 
 # check if wordpress is already configured
-if [ -f ./wp-config.php ]
+if [ -f /var/www/html/wp-config.php ];
 then
 	echo "wordpress already configured"
 else
 
-# install wp-cli
-# wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-# chmod +x wp-cli.phar
-# mv wp-cli.phar /usr/local/bin/wp
+cd /var/www/html
 
-# edit the wp-config.php file
-# sed -i "s/username_here/${MARIA_USER}/g" /var/www/html/wp-config.php
-# sed -i "s/password_here/${MARIA_PASSWORD}/g" /var/www/html/wp-config.php
-# sed -i "s/localhost/mariadb/g" /var/www/html/wp-config.php
-# sed -i "s/database_name_here/${MARIA_DATABASE}/g" /var/www/html/wp-config.php
+wp config create --path=/var/www/html \
+    --dbname="${MARIA_DATABASE}" \
+    --dbuser="${MARIA_USER}" \
+    --dbpass="${MARIA_PASSWORD}" \
+    --dbhost="mariadb" --allow-root
 
-# Éditer le fichier wp-config.php avec les variables d'environnement
-# wp config create --path=/var/www/html \
-#     --dbname="${MARIA_DATABASE}" \
-#     --dbuser="${MARIA_USER}" \
-#     --dbpass="${MARIA_PASSWORD}" \
-#     --dbhost="mariadb" --allow-root
-
-# # Installation de WordPress
-# wp core install --path=/var/www/html \
-#     --url="http://localhost" \
-#     --title="My WordPress" \
-#     --admin_user="admin" \
-#     --admin_password="admin_password" \
-#     --admin_email="admin@example.com" --allow-root
+wp core install --path=/var/www/html \
+    --url="http://localhost" \
+    --title="My WordPress" \
+    --admin_user="admin" \
+    --admin_password="admin_password" \
+    --admin_email="admin@example.com" --allow-root
 
 wp core download --allow-root
 
