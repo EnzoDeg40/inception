b57eda9445e1fcfba205dbbaf24470992c6ee040
Enzo Degraeve <35306710+EnzoDeg40@users.noreply.github.com>
2024-11-20 13:31:47 +0100

    Add IF NOT EXISTS on setup mariadb

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -5,8 +5,8 @@ set -xe
 
 service mariadb start
 
-mariadb -e "CREATE DATABASE ${MARIA_DATABASE};"
-mariadb -e "CREATE USER '${MARIA_USER}'@'%' IDENTIFIED BY '${MARIA_PASSWORD}';"
+mariadb -e "CREATE DATABASE IF NOT EXISTS ${MARIA_DATABASE};"
+mariadb -e "CREATE USER IF NOT EXISTS '${MARIA_USER}'@'%' IDENTIFIED BY '${MARIA_PASSWORD}';"
 mariadb -e "GRANT ALL PRIVILEGES ON ${MARIA_DATABASE}.* TO '${MARIA_USER}'@'%';"
 mariadb -e "FLUSH PRIVILEGES;"
 
